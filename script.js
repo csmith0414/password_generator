441302d9bc37594d222c0bd42d3a8ffd891af179
Chad Smith <cds021@yahoo.com>
2023-07-31 08:34:29 -0400

    updating js

--- script.js
+++ script.js
@@ -11,62 +11,72 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var number = [0,1,2,3,4,5,6,7,8,9];
-  var special = ["!","@","#","$","%","^","&","*","|","-","_","?",".",":",";","?","+","=","[","{","]","}","`","~"];
+  var size = lengthInput()
+  if (size === null) {
+    return ""
+  }
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var special = ["!", "@", "#", "$", "%", "^", "&", "*", "|", "-", "_", "?", ".", ":", ";", "?", "+", "=", "[", "{", "]", "}", "`", "~"];
   var options = []
-  let pick = window.prompt ("Choose length of password. Must be between 8 and 128 characters")
+  var islowerCase = true
+  var isupperCase = true
+  var isnumber = true
+  var isspecial = true
 
- 
   if (window.confirm("Does the password need lower case letters?")) {
-      options = options.concat(lowerCase)
-    } 
-    else if ((window.confirm === false)) {
-
-    }
+    options = options.concat(lowerCase)
+  }
+ else {
+    islowerCase = false
+  }
 
   if (window.confirm("Does the password need upper case letters?")) {
     options = options.concat(upperCase);
-  } 
-  else if ((window.confirm === false)) {
-
+  }
+  else {
+    isupperCase = false
   }
 
   if (window.confirm("Does the password need numbers?")) {
     options = options.concat(number);
-  } 
-  else if ((window.confirm === false)) {
-
+  }
+  else {
+    isnumber = false
   }
 
   if (window.confirm("Does the password need special characters?")) {
     options = options.concat(special);
-  } 
-  else if ((window.confirm === false)) {
-
   }
+  else {
+    isspecial = false
+  }
+if (islowerCase === false && isupperCase === false && isnumber === false && isspecial === false){
+  alert("You have to select at least one of the options")
+  return ""
+}
+return newPassword(options, size)
+}
 
-  function lengthInput(pick) {
-    if (pick >= 8 && pick <= 128) {
-       console.log(pick)
-       return pick
-    }
-    else {
+function lengthInput(pick) {
+  var pick = window.prompt("Please select length of password between 8 and 128 characters.")
+  if (pick >= 8 && pick <= 128) {
+    console.log(pick)
+    return pick
+  }
+  else {
     alert("Please enter a number between 8 and 128")
     return null
-    }
- }
-function options () {
-  var options = [];
-  for (key in charset) {
-    if (window.confirm == true) {
-       result = result.concat();
-       console.log(result);
-    }
- }
-} 
-  return "This is the password"
+  }
+}
+function newPassword(options, size) {
+  var finalPassword = ""
+  for (var i = 0; i < size; i++) {
+    var index = Math.floor(Math.random() * options.length)
+    finalPassword = finalPassword + options[index]
+  }
+  return finalPassword
 }
 
 // Add event listener to generate button
